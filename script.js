5108e98ede59b0095327abd9751612a8dbd3d577
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-03-09 04:31:03 -0800

    Added basic functionality to number buttons and the clear button. Also tweaked some id and classes.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+// Display
+let prevOperant = document.querySelector('.prev-operand');
+let currentOperant = document.querySelector('.current-operand');
+
+const clear = () => {
+  prevOperant.textContent = '';
+  currentOperant.textContent = '';
+};
+
+const deleteNum = () => {
+  currentOperant.textContent.toString().slice(0, -1);
+};
+
+document.querySelector('.delete-btn').addEventListener('click', () => {
+  deleteNum();
+});
+
+document.querySelector('.clear-btn').addEventListener('click', () => {
+  clear();
+});
+
+// Logic Buttons
+let logicBtns = document.querySelectorAll('.logic-btn');
+
+logicBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    currentOperant.textContent += btn.textContent;
+  });
+});
+
+let logicExits = document.querySelectorAll('.logic-exit');
+
+logicExits.forEach(newBtn => {
+  newBtn.addEventListener('click', () => {
+    currentOperant.textContent += newBtn.textContent;
+    prevOperant.textContent = currentOperant.textContent;
+    currentOperant.textContent = '';
+  });
+});
